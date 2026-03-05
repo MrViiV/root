eaa28550df4502b010c282b80d6912a67a6b1011
MrViiV <94287115+MrViiV@users.noreply.github.com>
2026-03-06 00:07:24 +0530

    [tmva][sofie] Export MakePyTorchBatchNorm2D from layers package"

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tmva/_sofie/_parser/_pytorch/layers/__init__.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tmva/_sofie/_parser/_pytorch/layers/__init__.py
@@ -1,7 +1,9 @@
 from .elu import MakePyTorchELU
 from .maxpool2d import MakePyTorchMaxPool2D
+from .batchnorm2d import MakePyTorchBatchNorm2D
 
 __all__ = [
     "MakePyTorchELU",
     "MakePyTorchMaxPool2D",
+    "MakePyTorchBatchNorm2D"
 ]
