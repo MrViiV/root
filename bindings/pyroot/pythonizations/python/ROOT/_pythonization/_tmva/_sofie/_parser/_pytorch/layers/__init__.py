858246ed47c131c03f0212ffb4559ff05a22d5fb
MrViiV <mrviiv9@root-build.asia-south1-a.c.project-00b8f24f-2d9a-46f4-8b9.internal>
2026-03-05 20:01:41 +0000

    [tmva][sofie] Export MakePyTorchRNN from layers package

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tmva/_sofie/_parser/_pytorch/layers/__init__.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tmva/_sofie/_parser/_pytorch/layers/__init__.py
@@ -1,9 +1,11 @@
 from .elu import MakePyTorchELU
 from .maxpool2d import MakePyTorchMaxPool2D
 from .batchnorm2d import MakePyTorchBatchNorm2D
+from .rnn import MakePyTorchRNN
 
 __all__ = [
     "MakePyTorchELU",
     "MakePyTorchMaxPool2D",
-    "MakePyTorchBatchNorm2D"
+    "MakePyTorchBatchNorm2D",
+    "MakePyTorchRNN"
 ]
